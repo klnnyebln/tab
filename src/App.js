1b6998a7cce3060ee3e0f83362abd1352eaca82c
klnnyebln <kyb5597@daum.net>
2023-12-12 11:34:02 +0900

    finish

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+function NumberGame() {
+  const [randomNumber] = useState(Math.floor(Math.random() * 100) + 1);
+  const [guess, setGuess] = useState('');
+  const [attempts, setAttempts] = useState(0);
+  const [result, setResult] = useState('');
+
+  const tryGuess = () => {
+    const numGuess = parseInt(guess);
+    if (isNaN(numGuess)) {
+      setResult("숫자를 입력하세요.");
+    } else {
+      setAttempts(previousAttempts => previousAttempts + 1);
+
+      if (numGuess === randomNumber) {
+        setResult("정답입니다!");
+      } else if (numGuess < randomNumber) {
+        setResult("Up!");
+      } else {
+        setResult("Down!");
+      }
+
+      if (attempts === 4) { 
+        setResult(previousResult => previousResult + " 게임 끝!");
+      }
+    }
+    setGuess('');
+  }
+
+  const handleOnKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      tryGuess();
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    /*html*/
+    <div>
+      <h1>숫자 게임</h1>
+      <p>1 ~ 100 사이의 숫자를 맞추시오.</p>
+      <input 
+        type="number" 
+        value={guess}
+        onChange={e => setGuess(e.target.value)}
+        onKeyDown = {handleOnKeyDown}
+        disabled={attempts >= 5}
+      />
+      <button onClick={tryGuess} disabled={attempts >= 5}>Click!</button>
+      <p>{result}</p>
     </div>
   );
 }
 
-export default App;
+
+export default NumberGame;
